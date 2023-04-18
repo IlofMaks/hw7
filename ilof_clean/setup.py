3fb5de879bbb30ca0f52662bc53bbbded982da4b
IlofMaks <121128704+IlofMaks@users.noreply.github.com>
2023-04-18 12:03:45 +0300

    Add files via upload

---
+++ ilof_clean/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_namespace_packages
+setup(
+    name = 'ilof_clean',
+    version = '0.0.1',
+    description = 'clean folder and translate file name',
+    autor = 'Ilof Maks',
+    autor_email = 'onischenkoalex94@gmail.com',
+    packages = ['ilof_clean'],
+    install_requires = [],
+    python_requires = '>=3.5',
+    include_package_data = True
+)
